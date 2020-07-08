6d441be1fe596f4d4e90c0117c3a1f1e1bde12c2
Nathan Strong <nathanstrong.fx@gmail.com>
2020-07-07 21:11:32 -0700

    Finished project layout. Finished section 1.1

--- 01-understanding-this/01-this-keyword-primer/src/app.ts
+++ 01-understanding-this/01-this-keyword-primer/src/app.ts
@@ -1,1 +1,24 @@
-console.log('cool!')
+// Function
+function myFunction() {
+    console.log('Function', this);
+}
+
+myFunction()
+
+// Object literal
+const myObj = {
+    myMethod() {
+        console.log('Object', this)
+    }
+}
+myObj.myMethod()
+
+// Classes
+class MyClass {
+    myMethod() {
+        console.log('Class', this)
+    }
+}
+
+const myInstance = new MyClass()
+myInstance.myMethod()
