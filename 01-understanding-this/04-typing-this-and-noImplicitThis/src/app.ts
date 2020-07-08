8fb5816b1210dbe45ce3b01ba6e54e4db263f714
Nathan Strong <nathanstrong.fx@gmail.com>
2020-07-08 05:52:17 -0700

    Finished section 1

--- 01-understanding-this/04-typing-this-and-noImplicitThis/src/app.ts
+++ 01-understanding-this/04-typing-this-and-noImplicitThis/src/app.ts
@@ -1,1 +1,10 @@
-console.log('cool!')
+window.onload = () => {
+    const elem = document.querySelector('.click') as HTMLAnchorElement
+
+    function handleClick(this: HTMLAnchorElement, event: Event) {
+        event.preventDefault()
+        console.log(this)
+    }
+
+    elem.addEventListener('click', handleClick, false)
+}
