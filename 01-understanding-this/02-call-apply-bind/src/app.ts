63929972f6415e985691b2f0ada561026075a7aa
Nathan Strong <nathanstrong.fx@gmail.com>
2020-07-07 22:06:28 -0700

    Finished project layout. Finished section 1.2

--- 01-understanding-this/02-call-apply-bind/src/app.ts
+++ 01-understanding-this/02-call-apply-bind/src/app.ts
@@ -1,1 +1,16 @@
-console.log('cool!')
+// Function
+function myFunction(text: string, text2: string = '') {
+    console.log('Function', this, text, text2);
+}
+
+// Object literal
+const myObj = {
+    myMethod() {
+        console.log('Object', this)
+    }
+}
+// myObj.myMethod()
+
+myFunction('abc')
+myFunction.call(myObj, 'abc', 'def')
+myFunction.call([])
