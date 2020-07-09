8638ebafabe46032df6e20bf0e461d076f7a441f
Nathan Strong <nathanstrong.fx@gmail.com>
2020-07-08 17:43:43 -0700

    Finished section 2 Setup section 3

--- 02-type-queries/02-keyof-index-type-queries/src/app.ts
+++ 02-type-queries/02-keyof-index-type-queries/src/app.ts
@@ -1,1 +1,14 @@
-console.log('cool!')
+const person = {
+    name: 'Oliver',
+    age: 3
+};
+
+type Person = typeof person;
+type PersonKeys = keyof Person;
+
+type PersonTypes = Person[PersonKeys]
+
+const anotherPerson: Person = {
+    name: 'Maxwell',
+    age: 101
+};
--- 02-type-queries/03-keyof-generics-and-lookup-types/src/app.ts
+++ 02-type-queries/03-keyof-generics-and-lookup-types/src/app.ts
@@ -1,1 +1,20 @@
-console.log('cool!')
+const person = {
+    name: 'Oliver',
+    age: 3
+};
+
+type Person = typeof person;
+type PersonKeys = keyof Person;
+
+type PersonTypes = Person[PersonKeys]
+
+function getProperty<T, K extends keyof T>(obj: T, key: K) {
+    return obj[key];
+}
+
+const personName = getProperty(person, 'name');
+
+const anotherPerson: Person = {
+    name: 'Maxwell',
+    age: 101
+};
