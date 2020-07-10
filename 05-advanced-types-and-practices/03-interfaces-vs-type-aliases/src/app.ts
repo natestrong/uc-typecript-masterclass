74290aa2772c8d1f1d34619e2f8a6a09a05cc087
Nathan Strong <nathanstrong.fx@gmail.com>
2020-07-10 08:49:24 -0700

    Finished 5.3

--- 05-advanced-types-and-practices/03-interfaces-vs-type-aliases/src/app.ts
+++ 05-advanced-types-and-practices/03-interfaces-vs-type-aliases/src/app.ts
@@ -1,1 +1,24 @@
-console.log('cool!')
+interface Item {
+    name: string
+}
+
+interface Artist extends Item {
+    songs: string[];
+}
+
+interface Artist {
+    getSong(): number
+}
+
+type Artist2 = {
+    name: string
+} & Item;
+
+
+const newArtist: Artist = {
+  name: 'tom',
+  songs: ['cool', 'okay okay'],
+  getSong(): number {
+      return this.songs.length
+  }
+};
