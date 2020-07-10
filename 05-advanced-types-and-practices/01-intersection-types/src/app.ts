b32a776f9bf3be493dbe800fe1ca5d99e23af11f
Nathan Strong <nathanstrong.fx@gmail.com>
2020-07-09 21:16:12 -0700

    Setup section 5

--- 05-advanced-types-and-practices/01-intersection-types/src/app.ts
+++ 05-advanced-types-and-practices/01-intersection-types/src/app.ts
@@ -1,1 +1,32 @@
-console.log('cool!')
+interface Order {
+    id: string;
+    amount: number;
+    currency: string;
+}
+
+interface Stripe {
+    card: string;
+    cvc: string;
+}
+
+interface PayPal {
+    email: string;
+}
+
+type CheckoutCard = Order & Stripe;
+type CheckoutPayPay = Order & PayPal;
+
+// Can also create an Intersection Type on the fly..
+type CheckoutABC = Order & {name: string};
+
+const order: Order = {
+    id: 'xj28s',
+    amount: 100,
+    currency: 'USD'
+};
+
+const orderCard: CheckoutCard = {
+    ...order,
+    card: '1000 2000 3000 4000',
+    cvc: '123',
+};
