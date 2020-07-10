2cfd1f2d7fffc53c9f3951c6b88b6c680d3b60ef
Nathan Strong <nathanstrong.fx@gmail.com>
2020-07-10 10:14:14 -0700

    Finished Section 6

--- 06-generics-and-overloads/01-function-generics/src/app.ts
+++ 06-generics-and-overloads/01-function-generics/src/app.ts
@@ -1,1 +1,32 @@
-console.log('cool!')
+class Pizza {
+    constructor(
+        private name: string,
+        private price: number
+    ) {
+    }
+}
+
+class List<T> {
+    private list: T[] = [];
+
+    addItem(item: T): void {
+        this.list.push(item)
+    }
+
+    getList(): T[] {
+        return this.list;
+    }
+}
+
+const list = new List<Pizza>();
+
+list.addItem(new Pizza('pepperoni', 15));
+list.addItem(new Pizza('Razzle-Dazzle', 250));
+
+interface Coupon {
+    discount: number
+}
+
+const couponList = new List<Coupon>();
+
+couponList.addItem({discount: 10});
--- 06-generics-and-overloads/02-function-overloads/src/app.ts
+++ 06-generics-and-overloads/02-function-overloads/src/app.ts
@@ -1,1 +1,15 @@
-console.log('cool!')
+function reverse(str: string): string;
+function reverse<T>(arr: T[]): T[];
+function reverse<T>(stringOrArray: string | T[]): string | T[] {
+    if (typeof stringOrArray === 'string') {
+        return stringOrArray
+            .split('')
+            .reverse()
+            .join('')
+    }
+    return stringOrArray.slice().reverse()
+}
+
+reverse('Pepperoni');
+reverse(['bacon', 'mushrooms', 'sausage']);
+reverse([1, 2, 3]);
